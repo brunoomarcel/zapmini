78e84b79c7c0067064aba480f0b83a301109b2f5
Bruno Marcel <bruno.marcel@emdagro.com>
2025-02-19 16:05:36 -0300

    feat: :children_crossing: Implementação de botão de copiar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 import Button from "@/components/Button";
 import { useState } from "react";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   const [number, setNumber] = useState("");
@@ -29,53 +31,68 @@ export default function Home() {
     setFormSubmitted(true);
   };
 
+  const copySavedNumber = (savedNumber: string) => {
+    const number = navigator.clipboard.writeText(savedNumber);
+    console.log(number);
+    console.log('clkicou')
+  }
+
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
-      <div className="w-full max-w-sm bg-white p-6 rounded-lg shadow-lg">
-        <h1 className="text-2xl font-semibold text-center mb-4">Gerador de Link do WhatsApp</h1>
+    <>
+      <Header />
+      <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
+        <div className="w-full max-w-sm bg-white p-6 rounded-lg shadow-lg">
+          <h1 className="text-2xl font-semibold text-center mb-4">Gerador de Link do WhatsApp</h1>
 
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Digite o seu número</label>
-            <input
-              type="tel"
-              className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500 outline-none"
-              value={number}
-              onChange={handleNumberChange}
-              required
-              maxLength={11}
-              placeholder="(DDD) 9XXXX-XXXX"
-            />
-          </div>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <div>
+              <label className="block text-gray-700 font-medium mb-1">Digite o seu número</label>
+              <input
+                type="tel"
+                className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500 outline-none"
+                value={number}
+                onChange={handleNumberChange}
+                required
+                maxLength={11}
+                placeholder="(DDD) XXXXX-XXXX"
+              />
+            </div>
 
-          <div>
-            <label className="block text-gray-700 font-medium mb-1">Mensagem Inicial (opcional)</label>
-            <input
-              type="text"
-              className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500 outline-none"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              placeholder="Digite uma mensagem..."
-            />
-          </div>
+            <div>
+              <label className="block text-gray-700 font-medium mb-1">Mensagem Inicial (opcional)</label>
+              <input
+                type="text"
+                className="w-full border border-gray-300 rounded-md p-2 focus:ring-2 focus:ring-blue-500 outline-none"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Digite uma mensagem..."
+              />
+            </div>
 
-          <Button />
-        </form>
+            <Button />
+          </form>
 
-        {formSubmitted && savedNumber && (
-          <div className="mt-6 p-4 bg-green-100 border border-green-400 rounded-lg text-center">
-            <h2 className="text-lg font-semibold text-green-700">Seu link do WhatsApp:</h2>
-            <a 
-              href={savedNumber} 
-              target="_blank" 
-              rel="noopener noreferrer" 
-              className="text-blue-600 hover:underline break-all"
-            >
-              {savedNumber}
-            </a>
-          </div>
-        )}
+          {formSubmitted && savedNumber && (
+            <div className="mt-6 p-4 bg-green-100 border border-green-400 rounded-lg text-center">
+              <h2 className="text-lg font-semibold text-green-700">Seu link do WhatsApp:</h2>
+              <a 
+                href={savedNumber} 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="text-blue-600 hover:underline break-all"
+              >
+                {savedNumber}
+              </a>
+              <button
+                className="mt-4 bg-gray-400 text-white rounded-md px-4 py-2 hover:bg-gray-500"
+                // onClick={copySavedNumber(savedNumber)}
+                onClick={() => copySavedNumber(savedNumber)}
+              >Copiar</button>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Footer(){
+    return(
+        <footer className="bg-green-500">
+            <p className="text-center p-2">Copyright © Todos os direitos reservados.</p>
+        </footer>
+    )
+}
