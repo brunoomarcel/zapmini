58e7cf589f1758480b2465f83f7bf05ef6da53b9
brunomarcel458 <109152699+brunomarcel458@users.noreply.github.com>
2025-05-04 21:31:06 -0300

    add tag adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,12 +32,7 @@ export default function RootLayout({
           `}
         </Script>
 
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1967208140287135"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+        <meta name="google-adsense-account" content="ca-pub-1967208140287135"/>
       </head>
       <body className={inter.className}>
         {/* Google Tag Manager (noscript) */}
